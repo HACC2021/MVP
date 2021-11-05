f4268b5046d4142408c0b989dfd559f500526c73
azureknight808 <zhien.w@gmail.com>
2021-11-04 21:48:00 -1000

    Added ability to sort for admin.html

---
+++ public/js/list_func.js
@@ -0,0 +1,54 @@
+/*
+Allows user to move up/down the queue with dragging
+https://betterprogramming.pub/create-a-sortable-list-with-draggable-items-using-javascript-9ef38f96b258
+*/
+var list = document.getElementById('list')
+var base, randomized, dragging, draggedOver;
+var isRight = 'Not In Order!';
+
+const genRandom = (array) => {
+    base = array.slice()
+    randomized = array.sort(() => Math.random() - 0.5)
+    if (randomized.join("") !== base.join("")){
+        renderItems(randomized)
+     } else {
+       //recursion to account if the randomization returns the original array
+       genRandom()
+     }
+  }
+
+const renderItems = (data) =>{
+    document.getElementById('isRight').innerText = isRight;
+    list.innerText = '';
+    data.forEach(item=>{
+      var node = document.createElement("li"); 
+      node.draggable = true;
+      node.addEventListener('drag', setDragging); 
+      node.addEventListener('dragover', setDraggedOver);
+      node.addEventListener('drop', compare);
+      node.innerText = item;
+      list.appendChild(node);
+    })
+  }
+
+  const setDragging = (e) =>{
+    dragging = parseInt(e.target.innerText)
+  }
+
+  function setDraggedOver(e) {
+    e.preventDefault();
+    draggedOver = parseInt(e.target.innerText)
+  }
+
+  
+const compare = (e) =>{
+    var index1 = randomized.indexOf(dragging);
+    var index2 = randomized.indexOf(draggedOver);
+    randomized.splice(index1, 1)
+    randomized.splice(index2, 0, dragging)
+   
+    isRight = randomized.join("") === base.join("") 
+      ? 'In Order!': 'Not In Order!'
+    
+    renderItems(randomized)
+  };
